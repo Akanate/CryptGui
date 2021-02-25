7cc8011fe5e17e20af89586e50ad8a9ae1505fd3
Akanate <48154623+Akanate@users.noreply.github.com>
2021-02-25 18:56:04 +0000

    Update CryptGUI.py

--- CryptGUI.py
+++ CryptGUI.py
@@ -10,6 +10,7 @@ from PyQt5.QtWidgets import QFormLayout
 from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QPlainTextEdit
 from PyQt5 import QtGui
 import time
 """Start of code"""
@@ -20,16 +21,23 @@ class CryptGui(QDialog):
         self.setWindowTitle('Encryptor')
         self.setWindowIcon(QtGui.QIcon('index.png'))
         self.msg = QLabel('')
+        self.resize(500,200)
+        self.file_array = []
         Dialog_layout = QVBoxLayout()
         Text_layout = QFormLayout()
         layout = QVBoxLayout()
-        self.textbox1 = QLineEdit(self)
+        self.textbox1 = QPlainTextEdit(self)
+        self.textbox1.setReadOnly(True)
+        self.textbox1.resize(20,20)
         self.path_button = QPushButton('Select Path to encrypt/decrypt')
         self.textbox2 = QLineEdit(self)
         self.key_button = QPushButton('Select Key Path')
         layout.addWidget(self.key_button)
         Text_layout.addRow('Path:',self.textbox1)
         Text_layout.addWidget(self.path_button)
+        self.clear_button = QPushButton('Clear Paths')
+        self.clear_button.clicked.connect(self.clear)
+        Text_layout.addWidget(self.clear_button)
         self.path_button.clicked.connect(self.select_path)
         Text_layout.addRow('Key Path:',self.textbox2)
         Text_layout.addWidget(self.key_button)
@@ -49,69 +57,92 @@ class CryptGui(QDialog):
         self.setLayout(Dialog_layout)
 
     def encrypt(self):
-        if self.textbox1.text() == None:
+        if self.file_array == []:
             self.msg.setText('Specify a path!!!')
             return
         if self.textbox2.text() == None:
             self.msg.setText('Specify a key to use!!!')
             return
-        path = self.textbox1.text()
         key_path = self.textbox2.text()
-        if os.path.exists(key_path) and os.path.exists(path):
-            try:
-                _path = open(path,'r')
-                contents = _path.read()
-                key_path = open(key_path,'rb')
-                key_contents = key_path.read()
-                k = f.Fernet(key_contents)
-                encoded_contents = contents.encode()
-                crypt = k.encrypt(encoded_contents)
-                _clear = open(path,'wb')
-                _clear.write(crypt)
-                _clear.close()
-                self.msg.setText(f'Encrypted file {path}')
-                self.textbox1.setText('')
-            except Exception as e:
-                self.msg.setText(f'An error has occured {e}')
-                self.textbox1.setText('')
+        if os.path.exists(key_path) == False:
+            self.msg.setText('The Encryption Key Path Does Not Exist')
+        print(self.file_array)
+        for i in range(len(self.file_array)):
+            print(i)
+            if os.path.exists(self.file_array[i]):
+                try:
+                    _path = open(self.file_array[i],'r')
+                    contents = _path.read()
+                    key_path = open(self.textbox2.text(),'rb')
+                    key_contents = key_path.read()
+                    k = f.Fernet(key_contents)
+                    encoded_contents = contents.encode()
+                    crypt = k.encrypt(encoded_contents)
+                    _clear = open(self.file_array[i],'wb')
+                    _clear.write(crypt)
+                    _clear.close()
+                    x=[i+'\n' for i in self.file_array]
+                    t=''.join(x)
+                    self.msg.setText(f'Encrypted file {t}')
+                    print('Hit')
+                    self.textbox1.setPlainText('')
+                except Exception as e:
+                    self.msg.setText(f'An error has occured {e}')
+                    return
+            else:
+                self.msg.setText('That path does not exist')
+                self.textbox1.setPlainText('')
+                self.file_array = []
                 return
-        else:
-            self.msg.setText('That path does not exist')
-            self.textbox1.setText('')
-            self.textbox2.setText('')
-            return
+        self.msg.setText(f'Encrypted {len(self.file_array)} files')
+        self.file_array = []
+        self.textbox1.setPlainText('')
 
     def decrypt(self):
+        if self.file_array == []:
+            self.msg.setText('Specify a path!!!')
+            return
+        if self.textbox2.text() == None:
+            self.msg.setText('Specify a key to use!!!')
+            return
+        print(self.file_array)
         key = self.textbox2.text()
-        path = self.textbox1.text()
-        if os.path.exists(path):
-            try: 
-                _keypath = open(f'{key}','rb')
-                key = _keypath.read()
-                k2 = f.Fernet(key)
-                _path = open(path,"r")
-                contents = _path.read()
-                _path.close()
-                contents = contents.encode()
-                decrypt = k2.decrypt(contents)
-                decoded = decrypt.decode()
-                overwrite = open(path,'w+')
-                overwrite.write(decoded)
-                overwrite.close()
-                self.msg.setText(f'Decrypted {path}')
-                self.textbox1.setText('')
-                self.textbox2.setText('')
-            except Exception as e:
-                self.msg.setText(f'An error has occured {e}')
-                self.textbox1.setText('')
-        else:
-            self.msg.setText('This path does not exist.')
+        if os.path.exists(key) == False:
+            self.msg.setText('The Encryption Key Path Does Not Exist')
+        for i in range(len(self.file_array)):
+            print(i)
+            if os.path.exists(self.file_array[i]):
+                try: 
+                    _keypath = open(f'{key}','rb')
+                    key1 = _keypath.read()
+                    k2 = f.Fernet(key1)
+                    _path = open(self.file_array[i],"r")
+                    contents = _path.read()
+                    _path.close()
+                    contents = contents.encode()
+                    decrypt = k2.decrypt(contents)
+                    decoded = decrypt.decode()
+                    overwrite = open(self.file_array[i],'w+')
+                    overwrite.write(decoded)
+                    overwrite.close()
+                    self.msg.setText(f'Decrypted {self.file_array[i]}')
+                except Exception as e:
+                    self.msg.setText(f'An error has occured {e}')
+                    self.textbox1.setPlainText('')
+            else:
+                self.msg.setText('This path does not exist.')
+                return
+        self.msg.setText(f'Decrypted {len(self.file_array)} files')
+        self.textbox1.setPlainText('')
+        self.file_array = []
 
     def select_path(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         filename,_ = QFileDialog.getOpenFileName(None, "Open a File", "","Text Files (*.txt)",options=options)
-        self.textbox1.setText(filename)
+        self.file_array.append(str(filename))
+        x = [i+'\n' for i in self.file_array]
+        self.textbox1.setPlainText(''.join(set(x)))
 
     def select_key_path(self):
         options = QFileDialog.Options()
@@ -127,7 +158,10 @@ class CryptGui(QDialog):
             e.write(key)
         self.msg.setText(f'Key Generated to file {file_name}.txt will automatically use this key unless new one generated.')
         self.textbox2.setText(f'{file_name}.txt')
-
+    
+    def clear(self):
+        self.textbox1.setPlainText('')
+        self.file_array = []
 
 
 if __name__ == '__main__':
@@ -135,4 +169,3 @@ if __name__ == '__main__':
     Encrypt = CryptGui()
     Encrypt.show()
     sys.exit(app.exec_())
-
